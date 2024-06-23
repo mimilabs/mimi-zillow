505f3ed027deef1457eed6c4d4fcb318e6aaa4ce
yubinmimi <yubinmimi>
2024-06-23 23:28:58 +0000

    new mimi variables added, and disambiguated zcta and zip

--- ingest.py
+++ ingest.py
@@ -1,23 +1,5 @@
 # Databricks notebook source
-!pip install tqdm
-
-# COMMAND ----------
-
-import pandas as pd
-from pathlib import Path
-import datetime
-from dateutil.parser import parse
-from tqdm import tqdm
-import re
-
-# COMMAND ----------
-
-def camel_to_underscore(text):
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', text).lower()
-
-def change_header(header_org):
-    return [camel_to_underscore(re.sub(r'\W+', '', column))
-            for column in header_org]
+# MAGIC %run /Workspace/Repos/yubin.park@mimilabs.ai/mimi-common-utils/ingestion_utils
 
 # COMMAND ----------
 
@@ -31,21 +13,12 @@ for file in Path("/Volumes/mimi_ws_1/zillow/src").glob("homevalue_zipcode*"):
     file_lst.append(file)
 # sort
 file_lst = sorted(file_lst)
-
-# COMMAND ----------
-
-pdf = pd.read_csv(file_lst[-1], dtype={"RegionID": str,
-                                       "RegionName": str})
-
-# COMMAND ----------
-
+file = file_lst[-1]
+pdf = pd.read_csv(file, dtype={"RegionID": str, "RegionName": str})
 col_set1 = pdf.columns[:9]
 col_set2 = pdf.columns[9:]
-
-# COMMAND ----------
-
 pdf_lst = []
-for colname in tqdm(col_set2):
+for colname in col_set2:
     pdf_t = pdf.loc[:,col_set1]
     pdf_t["date"] = parse(colname).date()
     pdf_t["value"] = pdf[colname]
@@ -53,19 +26,16 @@ for colname in tqdm(col_set2):
     pdf_t = pdf_t.dropna(subset=["value"])
     pdf_lst.append(pdf_t)
 pdf_full = pd.concat(pdf_lst)
-ifd = parse(file_lst[-1].stem[-8]).date()
-pdf_full["_input_file_date"] = ifd
-pdf_full = pdf_full.rename(columns={"region_i_d": "region_id",
-                         "region_name": "zcta"})
-pdf_full = pdf_full.drop(columns=["region_type"])    
-
-# COMMAND ----------
-
+pdf_full["mimi_src_file_date"] = parse(file.stem[-8:]).date()
+pdf_full["mimi_src_file_name"] = file.name
+pdf_full["mimi_dlt_load_date"] = datetime.today().date()
+pdf_full = pdf_full.rename(columns={"region_i_d": "region_id", "region_name": "zip"})
+pdf_full = pdf_full.drop(columns=["region_type"])
 df = spark.createDataFrame(pdf_full)
 (df.write
     .format('delta')
     .mode("overwrite")
-    .saveAsTable("mimi_ws_1.zillow.homevalue_zcta"))
+    .saveAsTable("mimi_ws_1.zillow.homevalue_zip"))
 
 # COMMAND ----------
 
@@ -79,21 +49,12 @@ for file in Path("/Volumes/mimi_ws_1/zillow/src").glob("rentals_zipcode*"):
     file_lst.append(file)
 # sort
 file_lst = sorted(file_lst)
-
-# COMMAND ----------
-
-pdf = pd.read_csv(file_lst[-1], dtype={"RegionID": str,
-                                       "RegionName": str})
-
-# COMMAND ----------
-
+file = file_lst[-1]
+pdf = pd.read_csv(file, dtype={"RegionID": str, "RegionName": str})
 col_set1 = pdf.columns[:9]
 col_set2 = pdf.columns[9:]
-
-# COMMAND ----------
-
 pdf_lst = []
-for colname in tqdm(col_set2):
+for colname in col_set2:
     pdf_t = pdf.loc[:,col_set1]
     pdf_t["date"] = parse(colname).date()
     pdf_t["value"] = pdf[colname]
@@ -101,20 +62,14 @@ for colname in tqdm(col_set2):
     pdf_t = pdf_t.dropna(subset=["value"])
     pdf_lst.append(pdf_t)
 pdf_full = pd.concat(pdf_lst)
-ifd = parse(file_lst[-1].stem[-8]).date()
-pdf_full["_input_file_date"] = ifd
+pdf_full["mimi_src_file_date"] = parse(file.stem[-8:]).date()
+pdf_full["mimi_src_file_name"] = file.name
+pdf_full["mimi_dlt_load_date"] = datetime.today().date()
 pdf_full = pdf_full.rename(columns={"region_i_d": "region_id",
-                         "region_name": "zcta"})
+                         "region_name": "zip"})
 pdf_full = pdf_full.drop(columns=["region_type"])
-
-# COMMAND ----------
-
 df = spark.createDataFrame(pdf_full)
 (df.write
     .format('delta')
     .mode("overwrite")
-    .saveAsTable("mimi_ws_1.zillow.rent_zcta"))
-
-# COMMAND ----------
-
-
+    .saveAsTable("mimi_ws_1.zillow.rent_zip"))
